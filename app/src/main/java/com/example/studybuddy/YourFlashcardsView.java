11d12d248180e0e01b195fcd022b524e707a14ff
Theobald Rousset <trousset@terpmail.umd.edu>
2022-12-04 15:51:34 -0500

    Arrows for flashcards included. Edit icon added to res

--- app/src/main/java/com/example/studybuddy/YourFlashcardsView.java
+++ app/src/main/java/com/example/studybuddy/YourFlashcardsView.java
@@ -3,6 +3,7 @@ package com.example.studybuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,8 +25,8 @@ import java.util.ArrayList;
 public final class YourFlashcardsView extends AppCompatActivity {
     TextView cardContent;
     ArrayList<FlashcardModel> flashcards;
-    TextView rightArrow;
-    TextView leftArrow;
+    ImageButton rightArrow;
+    ImageButton leftArrow;
     TextView setName;
     TextView progress;
     FirebaseAuth mAuth;
